bfdad3b21f002e74831e9e6c33d16f4ce6a55b29
CfdTsubasa <78243094+CfdTsubasa@users.noreply.github.com>
2022-04-11 22:38:14 +0900

    Update duel.rb

--- duel.rb
+++ duel.rb
@@ -1,4 +1,4 @@
-def Battle(condition = 'first')
+def battle(condition = 'first')
   if condition == 'first'
     firstCall = 'じゃんけん…'
     call = 'ポイ！'
@@ -32,7 +32,7 @@ def judge(enemy_hand, my_hand)
   case enemy_hand
   when 0 # 0(グー)
     if my_hand == 0 # 0(グー)
-      Battle('draw')
+      battle('draw')
     elsif my_hand == 1 # 1(チョキ)
       fingerPointingGame('enemy_turn')
     elsif my_hand == 2 # 2(パー)
@@ -42,7 +42,7 @@ def judge(enemy_hand, my_hand)
     if my_hand == 0 # 0(グー)
       fingerPointingGame('my_turn')
     elsif my_hand == 1 # 1(チョキ)
-      Battle('draw')
+      battle('draw')
     elsif my_hand == 2 # 2(パー)
       fingerPointingGame('enemy_turn')
     end
@@ -52,39 +52,39 @@ def judge(enemy_hand, my_hand)
     elsif my_hand == 1 # 1(チョキ)
       fingerPointingGame('my_turn')
     elsif my_hand == 2 # 2(パー)
-      Battle('draw')
+      battle('draw')
     end
   end
 end
 
 def fingerPointingGame(turn)
   if turn == 'my_turn'
-    turnMsg = '指す方向:'
+    turn_msg = '指す方向:'
     result = 'あなたの勝ちです！'
   elsif turn == 'enemy_turn'
-    turnMsg = '向く方向:'
+    turn_msg = '向く方向:'
     result = 'あなたの負けです…'
   end
 
   puts 'あっち向いて…'
 
   directions = %W[\u4E0A \u53F3 \u4E0B \u5DE6]
-  puts turnMsg + '0(上)1(右)2(下)3(左)'
+  puts "#{turn_msg}  0(上)1(右)2(下)3(左)"
 
   my_direct = gets.to_i
   enemy_direct = rand(0..3)
 
   puts 'ホイ！'
   puts '==================='
-  p '相手:' + directions[enemy_direct]
-  p 'あなた:' + directions[my_direct]
+  p "相手: + #{directions[enemy_direct]}"
+  p "あなた: #{directions[my_direct]}"
   puts '==================='
 
   if my_direct == enemy_direct
     puts result
   else
-    Battle()
+    battle
   end
 end
 
-Battle()
+battle
